d99f85fc8916c60f20322d2eefeb2868eeacbdcd
Ismail-2828 <ismailmoshood1996@gmail.com>
2023-01-19 11:44:40 +0100

    added calculator.py

---
+++ calculator.py
@@ -0,0 +1,41 @@
+import cmath
+import sys
+from PyQt5.QtWidgets import *
+
+def dialog():
+    global mbox,A
+    mbox = QMessageBox()
+    A = a.text()
+    B = b.text()
+    C = c.text()
+
+    D=(B**2)-(4*A*C)
+    sol1=(-B-cmath.sqrt(D))/(2*A)
+    sol2=(-B+cmath.sqrt(D))/(2*A)
+    mbox.setWindowTitle("Answer")
+    mbox.setText(f"Solutions: {sol1},{sol2}")
+    mbox.exec_()
+    
+
+if __name__ =="__main__":
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(400,400)
+    w.setWindowTitle("CALCULATOR")
+    layout = QFormLayout()
+
+    a = QLineEdit()
+    b = QLineEdit()
+    c = QLineEdit()
+    layout.addRow("A:",a)
+    layout.addRow("B:",b)
+    layout.addRow("C:",c)
+
+    btn = QPushButton("CALCULATE")
+    btn.clicked.connect(dialog)
+    layout.addRow(btn)
+
+    w.setLayout(layout)
+    w.show()
+    
+
